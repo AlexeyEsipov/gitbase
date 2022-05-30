dc6b2c510dcca88fe9dd9e242e5edad690166bc2
AlexeyEsipov <a_esipov@list.ru>
2022-05-30 21:05:41 +0300

    Новая ветка, второе изменение

--- src/main/java/ru/job4j/Probe.java
+++ src/main/java/ru/job4j/Probe.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Probe {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World!");
     }
 
 }
